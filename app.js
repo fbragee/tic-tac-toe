82ef491f65b18e46f3d042ac268e763c8b21b3fc
fbragee <145098995+fbragee@users.noreply.github.com>
2024-02-19 10:31:02 +0000

    Added start game functionality, fixed some reference errors and added the tic tac toe grid

--- app.js
+++ app.js
@@ -5,15 +5,39 @@ const Gameboard = (() => {
 	const render = () => {
 		let boardHTML = '';
 		gameBoard.forEach((square, index) => {
-			boardHTML += `<div class='square' id='square-${index}> ${square} </div>`;
+			boardHTML += `<div class='square' id=square-${index}> ${square} </div>`;
 		});
+		document.querySelector('#board').innerHTML = boardHTML;
 	};
-	document.querySelector('#board').innerHTML = boardHTML;
 
 	return { render };
 })();
 
+const createPlayer = (name, mark) => {
+	return {
+		name,
+		mark,
+	};
+};
+
+const Game = (() => {
+	let players = [];
+	let currentPlayerIndex;
+	let gameOver;
+
+	const start = () => {
+		players = [
+			createPlayer(document.querySelector('#player1').value, 'X'),
+			createPlayer(document.querySelector('#player2').value, 'O'),
+		];
+		currentPlayerIndex = 0;
+		gameOver = false;
+		Gameboard.render();
+	};
+	return { start };
+})();
+
 const startButton = document.querySelector('#start-button');
 startButton.addEventListener('click', () => {
-	//Game.start();
+	Game.start();
 });
